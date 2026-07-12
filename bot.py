42415d6966f4b4754cba25ea355ecca442ac8e9c
artemwalker0914-spec <artemwalker0914@gmail.com>
2026-07-12 18:54:04 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -5,8 +5,8 @@ from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 
 # --- НАСТРОЙКИ (Замените на свои!) ---
-TOKEN = "ВАШ_ТОКЕН"                # Вставьте сюда ваш токен от @BotFather
-GROUP_ID = -1001234567890          # Вставьте ID вашей группы (отрицательное число!)
+TOKEN = "8639656890:AAHOfXP_GA7Ve7wQD1WNjQY3pV_U-4FhMD0"                # Вставьте сюда ваш токен от @BotFather
+GROUP_ID = -1003721858380          # Вставьте ID вашей группы (отрицательное число!)
 # -------------------------------------
 
 logging.basicConfig(
