3652041b96a57a25bcbf136e6744ad2b1d122cf4
artemwalker0914-spec <artemwalker0914@gmail.com>
2026-07-12 18:19:08 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 
 # --- НАСТРОЙКИ (Замените на свои!) ---
-TOKEN = 8639656890:AAHOfXP_GA7Ve7wQD1WNjQY3pV_U-4FhMD0  # Вставьте сюда ваш токен
+TOKEN = "8639656890:AAHOfXP_GA7Ve7wQD1WNjQY3pV_U-4FhMD0"  # Вставьте сюда ваш токен
 GROUP_ID = -1003721858380  # Вставьте ID вашей группы (это отрицательное число!)
 # -------------------------------------
 
